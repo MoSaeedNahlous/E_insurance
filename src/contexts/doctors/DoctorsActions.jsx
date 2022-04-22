594e6ee9dab20f8b6e192b3e9bdbd9b4b9dff4d2
Mo Saeed Nahlous <jay.haji041@gmail.com>
2022-04-22 17:56:36 +0300

    commm22

--- src/contexts/doctors/DoctorsActions.jsx
+++ src/contexts/doctors/DoctorsActions.jsx
@@ -1,96 +1,104 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const getDoctors = async (dispatch) => {
-    dispatch({ type: 'GET_DOCTORS_REQUEST' })
-    try {
-        const { data } = await axios.get(
-            'https://arcane-garden-68747.herokuapp.com/api/doctors', {
-                
-                headers: {
-                    "authorization":'Bearer '+ JSON.parse(localStorage.getItem('user')).token
-                }
-            }
-        )
-        dispatch({ type: "GET_DOCTORS_SUCCESS", payload: data })
-    } catch (error) {
-        dispatch({type:"GET_DOCTORS_FAIL",payload:error})
-    }
-}
+  dispatch({ type: 'GET_DOCTORS_REQUEST' });
+  try {
+    const { data } = await axios.get(
+      'https://arcane-garden-68747.herokuapp.com/api/doctors',
+      {
+        headers: {
+          authorization:
+            'Bearer ' + JSON.parse(localStorage.getItem('user')).token,
+        },
+      }
+    );
+    dispatch({ type: 'GET_DOCTORS_SUCCESS', payload: data });
+  } catch (error) {
+    dispatch({ type: 'GET_DOCTORS_FAIL', payload: error });
+  }
+};
 
-export const updateDoctor = async (data1,dispatch) => {
-    dispatch({ type: "UPDATE_DOCTOR_REQUEST"})
-    try {
-        
-        const { data } = await axios.put(
-            `https://arcane-garden-68747.herokuapp.com/api/doctors/${data1._id}/update`, data1 , {
-                
-                headers: {
-                    "authorization":'Bearer '+ JSON.parse(localStorage.getItem('user')).token
-                }
-            }
-        )
-        dispatch({ type: "UPDATE_DOCTOR_SUCCESS"})
-    } catch (error) {
-        console.log(error);
-        dispatch({type:"UPDATE_DOCTOR_FAIL",payload:error})
-    }
-}
+export const updateDoctor = async (data1, dispatch) => {
+  dispatch({ type: 'UPDATE_DOCTOR_REQUEST' });
+  try {
+    const { data } = await axios.put(
+      `https://arcane-garden-68747.herokuapp.com/api/doctors/${data1._id}/update`,
+      data1,
+      {
+        headers: {
+          authorization:
+            'Bearer ' + JSON.parse(localStorage.getItem('user')).token,
+        },
+      }
+    );
+    dispatch({ type: 'UPDATE_DOCTOR_SUCCESS' });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: 'UPDATE_DOCTOR_FAIL', payload: error });
+  }
+};
 
-export const changeActive = async (id,dispatch) => {
+export const changeActive = async (id, dispatch) => {
+  try {
+    const { data } = await axios.put(
+      `https://arcane-garden-68747.herokuapp.com/api/doctors/${id}/activeState`,
+      null,
+      {
+        headers: {
+          authorization:
+            'Bearer ' + JSON.parse(localStorage.getItem('user')).token,
+        },
+      }
+    );
+  } catch (error) {
+    dispatch({ type: 'UPDATE_DOCTOR_FAIL', payload: error });
+  }
+};
 
-    try {
-        const { data } = await axios.put(
-            `https://arcane-garden-68747.herokuapp.com/api/doctors/${id}/activeState`,null,{
-                headers: {
-                    "authorization":'Bearer '+JSON.parse(localStorage.getItem('user')).token
-                }
-            }
-        )
-    } catch (error) {
-        dispatch({type:"UPDATE_DOCTOR_FAIL",payload:error})
-    }
-}
-
-export const setCurrentDoctor = async (docObj,dispatch) => {
-    dispatch({ type: "SET_CURRENT_DOCTOR", payload: docObj })
-}
+export const setCurrentDoctor = async (docObj, dispatch) => {
+  dispatch({ type: 'SET_CURRENT_DOCTOR', payload: docObj });
+};
 
 export const clearCurrentDoctor = async (dispatch) => {
-    dispatch({ type: "SET_CURRENT_DOCTOR_RESET"})
-}
+  dispatch({ type: 'SET_CURRENT_DOCTOR_RESET' });
+};
 
-export const addVisit = async (data1,dispatch) => {
-    dispatch({ type: "UPDATE_DOCTOR_REQUEST"})
-    try {
-        const { data } = await axios.post(
-            `https://arcane-garden-68747.herokuapp.com/api/doctors/recordVisit`,data1,{
-                headers: {
-                    "authorization":'Bearer '+JSON.parse(localStorage.getItem('user')).token
-                }
-            }
-        )
-        dispatch({ type: "UPDATE_DOCTOR_SUCCESS"})
-    } catch (error) {
-        console.log(error.message);
-        dispatch({type:"UPDATE_DOCTOR_FAIL",payload:"User Reached his visits limit!"})
-    }
-}
+export const addVisit = async (data1, dispatch) => {
+  dispatch({ type: 'UPDATE_DOCTOR_REQUEST' });
+  try {
+    const { data } = await axios.post(
+      `https://arcane-garden-68747.herokuapp.com/api/doctors/recordVisit`,
+      data1,
+      {
+        headers: {
+          authorization:
+            'Bearer ' + JSON.parse(localStorage.getItem('user')).token,
+        },
+      }
+    );
+    dispatch({ type: 'UPDATE_DOCTOR_SUCCESS' });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({ type: 'UPDATE_DOCTOR_FAIL', payload: error });
+  }
+};
 
-export const getTotalFeesInDate = async (data1,dispatch) => {
-    dispatch({ type: "GET_TOTAL_FEES_REQUEST"})
-    try {
-        const { data } = await axios.post(
-            `https://arcane-garden-68747.herokuapp.com/api/doctors/totalFee`,data1,{
-                headers: {
-                    "authorization":'Bearer '+JSON.parse(localStorage.getItem('user')).token
-                }
-            }
-        )
-        dispatch({ type: "GET_TOTAL_FEES_SUCCESS", payload:data})
-    } catch (error) {
-        console.log(error.message);
-        dispatch({ type: "GET_TOTAL_FEES_FAIL", payload:error})
-        
-    }
-}
-
+export const getTotalFeesInDate = async (data1, dispatch) => {
+  dispatch({ type: 'GET_TOTAL_FEES_REQUEST' });
+  try {
+    const { data } = await axios.post(
+      `https://arcane-garden-68747.herokuapp.com/api/doctors/totalFee`,
+      data1,
+      {
+        headers: {
+          authorization:
+            'Bearer ' + JSON.parse(localStorage.getItem('user')).token,
+        },
+      }
+    );
+    dispatch({ type: 'GET_TOTAL_FEES_SUCCESS', payload: data });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({ type: 'GET_TOTAL_FEES_FAIL', payload: error });
+  }
+};
--- src/pages/registerPages/PharmacistReg.jsx
+++ src/pages/registerPages/PharmacistReg.jsx
@@ -1,67 +1,98 @@
-import { Alert } from '@mui/material'
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate,Link } from 'react-router-dom'
-import { doctorRegister, pharmacistRegister } from '../../contexts/Auth/AuthActions'
-import { AuthContext } from '../../contexts/Auth/AuthContext'
-
+import { Alert, CircularProgress } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import {
+  doctorRegister,
+  pharmacistRegister,
+} from '../../contexts/Auth/AuthActions';
+import { AuthContext } from '../../contexts/Auth/AuthContext';
 
 const PharmacistReg = () => {
-    const navigate = useNavigate()
-    const {dispatch,user,isLoading,error} = useContext(AuthContext)
-    const [data, setData] = useState({
-        name: "",
-        phone: "",
-        password: '',
-        address:''
-    })
+  const navigate = useNavigate();
+  const { dispatch, user, isLoading, error } = useContext(AuthContext);
+  const [data, setData] = useState({
+    name: '',
+    phone: '',
+    password: '',
+    address: '',
+  });
 
-    const onChangeHandler = (e) => {
-        setData({...data,[e.target.name]:e.target.value})
+  const onChangeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    pharmacistRegister(data, dispatch);
+  };
+
+  useEffect(() => {
+    dispatch({ type: 'LOGIN_RESET' });
+    if (!user || !user.isAdmin) {
+      navigate('/', { replace: true });
     }
-
-    const onSubmitHandler = (e) => {
-        e.preventDefault()
-        pharmacistRegister(data,dispatch)
-    }
-
-    useEffect(() => {
-        dispatch({type:'LOGIN_RESET'})
-        if (!user || !user.isAdmin) {
-          navigate('/',{replace:true})
-        }
-    }, [user])
-    
+  }, [user]);
 
   return (
-      <div>
-          { error && <Alert severity="error">{ error }</Alert>}
-          <form onSubmit={onSubmitHandler} >
-              <h2>Create Pharmacist Register</h2>
-                <div className="form-group">
-                    <input type="text" onChange={onChangeHandler} value={data.name} className="form-control" name='name' aria-describedby="emailHelp" placeholder="Enter name"/>
-                </div>
-            
-                <div className="form-group">
-                    <input type="password" onChange={onChangeHandler} value={data.password}  className="form-control" name='password'  placeholder="Enter password"/>
-              </div>
-              
-              <div className="form-group">
-                    <input type="text" onChange={onChangeHandler} value={data.phone}  className="form-control" name='phone' placeholder="Enter phone number"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" onChange={onChangeHandler} value={data.address} className="form-control" name='address'  placeholder="Enter address"/>
-                </div>
-                <div className="form-group">
-                  <button type="submit" className="btn btn-primary btn-lg"
-                      disabled={ isLoading }
-          >
-            {isLoading ? <CircularProgress /> :"Create"}</button>
-                </div>
-          </form >
-      </div>
-      
-          
-  )
-}
+    <div>
+      {error && <Alert severity='error'>{error}</Alert>}
+      <form onSubmit={onSubmitHandler}>
+        <h2>Create Pharmacist Register</h2>
+        <div className='form-group'>
+          <input
+            type='text'
+            onChange={onChangeHandler}
+            value={data.name}
+            className='form-control'
+            name='name'
+            aria-describedby='emailHelp'
+            placeholder='Enter name'
+          />
+        </div>
 
-export default PharmacistReg
+        <div className='form-group'>
+          <input
+            type='password'
+            onChange={onChangeHandler}
+            value={data.password}
+            className='form-control'
+            name='password'
+            placeholder='Enter password'
+          />
+        </div>
+
+        <div className='form-group'>
+          <input
+            type='text'
+            onChange={onChangeHandler}
+            value={data.phone}
+            className='form-control'
+            name='phone'
+            placeholder='Enter phone number'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            onChange={onChangeHandler}
+            value={data.address}
+            className='form-control'
+            name='address'
+            placeholder='Enter address'
+          />
+        </div>
+        <div className='form-group'>
+          <button
+            type='submit'
+            className='btn btn-primary btn-lg'
+            disabled={isLoading}
+          >
+            {isLoading ? <CircularProgress /> : 'Create'}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PharmacistReg;
